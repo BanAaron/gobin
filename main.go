1638a24775ff7c8b92e5a1e8fd2656dddc52f549
Aaron Barratt <aarontbarratt@gmail.com>
2024-11-16 00:13:15 +0000

    saving info files as json

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ const filesDir string = "./Trash/files"
 const infoDir string = "./Trash/info"
 
 type fileInfo struct {
-	UUID        string
+	UUID        uuid.UUID
 	FileName    string
 	FilePath    string
 	DeletedDate int64
@@ -26,28 +26,54 @@ func (fi fileInfo) String() string {
 	return fmt.Sprintf("%s %s %s %d %d", fi.UUID, fi.FileName, fi.FilePath, fi.DeletedDate, fi.FileSize)
 }
 
+func newFileInfo(fileName string) (fi fileInfo, err error) {
+	uid, err := uuid.NewUUID()
+	if err != nil {
+		return fi, fmt.Errorf("copyFileToTrash: Could not generate UUID. %s", err)
+	}
+	unixTime := time.Now().Unix()
+	filePath, err := filepath.Abs(fileName)
+	if err != nil {
+		return fi, fmt.Errorf("copyFileToTrash: Could not get absolute path. %s", err)
+	}
+	fi = fileInfo{uid, fileName, filePath, unixTime, 0}
+	return fi, nil
+}
+
+func (fi fileInfo) Marshall() (FileInfoJson []byte, err error) {
+	FileInfoJson, err = json.Marshal(fi)
+	if err != nil {
+		return []byte{}, fmt.Errorf("fileInfo.Marshall(): Could not marshal JSON. %s", err)
+	}
+	return FileInfoJson, nil
+}
+
+// buildInfoPath will create the file path for fileInfo objects
+//
+// for example: ./Trash/info/b5b8cc50-a3ad-11ef-b204-0a01357c9fb2.json
+func buildInfoPath(fi fileInfo) string {
+	return infoDir + "/" + fi.UUID.String() + ".json"
+}
+
 // copyFileToTrash will create a copy of a file in the Trash directory and then
 // remove the original file
 func copyFileToTrash(fileName string) (err error) {
 	// create uuid
-	uid, err := uuid.NewUUID()
+	fi, err := newFileInfo(fileName)
 	if err != nil {
-		return fmt.Errorf("copyFileToTrash: Could not generate UUID. %s", err)
+		return fmt.Errorf("copyFileToTrash: Could not create file info. %s", err)
 	}
-	// get unix time
-	unixTime := time.Now().Unix()
-	// get file path
-	filePath, err := filepath.Abs(fileName)
+
+	fileInfoJson, err := fi.Marshall()
 	if err != nil {
-		return fmt.Errorf("copyFileToTrash: Could not get absolute path. %s", err)
+		return fmt.Errorf("copyFileToTrash: Could not marshal file info. %s", err)
 	}
-	// create json
-	fi := fileInfo{uid.String(), fileName, filePath, unixTime, 0}
-	fiJson, err := json.Marshal(fi)
+
+	infoPath := buildInfoPath(fi)
+	err = os.WriteFile(infoPath, fileInfoJson, 0644)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("copyFileToTrash: Could not write file info. %s", err)
 	}
-	fmt.Println(fiJson)
 
 	return nil
 }
@@ -73,8 +99,11 @@ func main() {
 		fmt.Println("Please provide a file.")
 		return
 	}
-	fileName := args[1]
+	if len(args) != 3 {
+		fmt.Println("Only one file at a time for now.")
+	}
 
+	fileName := args[1]
 	err := copyFileToTrash(fileName)
 	if err != nil {
 		panic(err)
